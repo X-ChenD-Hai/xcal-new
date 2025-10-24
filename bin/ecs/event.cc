f035353f7c36811a3ca82d5a53de8479bf9638c4
X_ChenD_Hai <illuminatestar@foxmail.com>
2025-10-24 17:33:46 +0800

    feat(ecs): 添加事件处理示例
    在 `ecs` 模块中添加了一个 `event.cc` 文件，演示了如何使用事件总线发布事件和处理事件。
    - 定义了一个 `MyEvent` 结构体，包含一个整数值。
    - 使用 `ecs::EventBus` 创建事件总线。
    - 发布一个 `MyEvent` 事件，值为 11。
    - 使用 `event_bus.each` 方法注册了两个回调函数来处理 `MyEvent` 事件，并展示了不同类型的参数传递（常量和引用）。
    - 打印事件总线中 `MyEvent` 事件的数量。
    - 使用 `event_bus.clear` 清空了事件总线。
    
    此外，更新了 `xmake.lua` 文件，为新添加的 `event.cc` 文件配置了一个新的目标 `ecs-event`，确保其能够被编译。
    
    删除了 `xc.h` 文件，其中包含了一个旧版的实体组件系统实现。这个旧实现已经被新的 `ecs` 模块所取代。

---
+++ bin/ecs/event.cc
@@ -0,0 +1,30 @@
+#include <ecs/event_bus.hpp>
+#include <iostream>
+
+struct MyEvent {
+    int value;
+};
+using Call = void (*)(MyEvent&);
+int main(int argc, char* argv[]) {
+    ecs::EventBus event_bus;
+
+    event_bus.publish<MyEvent>(11);
+
+    event_bus.each(
+        [](MyEvent& event, int a) {
+            std::cout << "MyEvent value: " << event.value + a << std::endl;
+        },
+        12);
+    std::cout << "size " << event_bus.size<MyEvent>() << std::endl;
+    int s =11;
+    event_bus.each(
+        [&](MyEvent& event, int& a) {
+            std::cout << "MyEvent value: " << event.value + a << std::endl;
+            return true;
+        },
+        s);
+    std::cout << "size " << event_bus.size<MyEvent>() << std::endl;
+    event_bus.clear();
+
+    return 0;
+}
